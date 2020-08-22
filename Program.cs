f5c02cc2d516dd428580fb4a84e59e7ca52203ed
rawkingo <69311715+rawkingo@users.noreply.github.com>
2020-08-22 22:07:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;  
+  public class Fibonacci  
+   {  
+     public static void Main(string[] args)  
+      {  
+         int a=0,b=1,c,i,n;    
+         Console.Write("Write the needed numbers of Fibonacci: ");    
+         n = int.Parse(Console.ReadLine());  
+         Console.Write(a+" "+b+" ");    
+         for(i=2;i<n;++i)     
+         {    
+          c=a+b;    
+          Console.Write(c+" ");    
+          a=b;    
+          b=c;    
+         }    
+      }  
+   }  
